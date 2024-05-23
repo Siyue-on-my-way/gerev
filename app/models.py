a5ca86e1cd4645397426e76871f6d0baa2c2118b
Sean <1441416954@qq.com>
2024-05-23 16:18:56 +0800

    启动dock erdocker时不启动服务

--- app/models.py
+++ app/models.py
@@ -5,9 +5,11 @@ import torch
 
 import os
 print(os.listdir('../greve_model'))
+
 bi_encoder = SentenceTransformer('../greve_model/multi-qa-MiniLM-L6-cos-v1')
 
-cross_encoder_small = CrossEncoder('../greve_model/cross-encoder/ms-marco-TinyBERT-L-2-v2')
-cross_encoder_large = CrossEncoder('../greve_model/cross-encoder/ms-marco-MiniLM-L-6-v2')
+cross_encoder_small = CrossEncoder('../greve_model/ms-marco-TinyBERT-L-2-v2')
+cross_encoder_large = CrossEncoder('../greve_model/ms-marco-MiniLM-L-6-v2')
 
 qa_model = pipeline('question-answering', model='../greve_model/roberta-base-squad2')
+
